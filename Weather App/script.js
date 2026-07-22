d6240404a74ff67c68d454768e7b20814753ae3b
Yash Sathe <yashsathe1357@gmail.com>
2026-07-23 01:06:25 +0530

    Remove the cuurent date from the forecast

--- Weather App/script.js
+++ Weather App/script.js
@@ -86,7 +86,15 @@ async function updateWeatherInfo(city) {
 async function updateForecastsInfo(city){
   const forecastsData = await getFetchData('forecast',city);
   const timeTaken = '12:00:00';
-  const todayDate = new Date().toISOString().split('T')[0];
+  
+  function getLocalDateString(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+const todayDate = getLocalDateString(new Date());
  
   forecastItemContainer.innerHTML = ''
   forecastsData.list.forEach(forecastWeather =>{
