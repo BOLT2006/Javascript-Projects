9b2c47a6fedb0d906fac60064094162ef3fbd67c
Yash Sathe <yashsathe1357@gmail.com>
2026-07-27 19:22:44 +0530

    Started with Snake Game and completd the board part

---
+++ SNAKE Game/script.js
@@ -0,0 +1,41 @@
+const board = document.querySelector('.board');
+const blockHeight = 50;
+const blockWidth = 50;
+
+/* Board Logic*/
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight); 
+
+// Give the coordinate of each block 
+const blocks = [];
+
+// Coordinate of Snake
+const snake = [{
+    x:1,y:3
+},{
+    x:1,y:4
+},{
+    x:1,y:5
+}]
+
+let direction = 'right';
+
+for( let row = 0; row < rows; row++){
+    for(let col = 0; col < cols; col++){
+        const block = document.createElement('div');
+        block.classList.add('block');
+        board.appendChild(block);
+        block.innerText = `${row}-${col}`;
+        blocks[`${row}-${col}`] = block;
+    }
+}
+
+function render(){
+    snake.forEach(segment =>{
+        blocks[`${segment.x}-${segment.y}`].classList.add('fill');
+    })
+};
+
+setInterval(()=>{
+    render()
+},300);
