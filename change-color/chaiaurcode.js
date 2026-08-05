cf68c422c695afff791ae380348228c7f8b1453f
Yash Sathe <yashsathe1357@gmail.com>
2026-08-05 19:33:10 +0530

    Used Switch statement in case of if else

--- change-color/chaiaurcode.js
+++ change-color/chaiaurcode.js
@@ -3,9 +3,7 @@ const body = document.querySelector("body")
 
 buttons.forEach((button) =>{
     button.addEventListener('click' , (e) =>{
-        // console.log(e);
-        // console.log(e.target);
-        
+        /*
         if(e.target.id === 'grey'){
             body.style.backgroundColor = 'grey'
         }
@@ -18,5 +16,26 @@ buttons.forEach((button) =>{
         if(e.target.id === 'yellow'){
             body.style.backgroundColor = 'yellow'
         }
+        */
+       const id = e.target.id;
+       
+       switch (id) {
+        case 'grey':
+            body.style.backgroundColor = 'grey'
+            break;
+        case 'white':
+            body.style.backgroundColor = 'white'
+            break;
+        case 'blue':
+            body.style.backgroundColor = 'blue'
+            break;
+        case 'yellow':
+            body.style.backgroundColor = 'yellow'
+            break;
+       
+        default:
+            break;
+       }
+       
     })
 })
