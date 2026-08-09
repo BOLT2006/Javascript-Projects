c6dfbbb06cfcb592732babf2430843a3afef5c12
Yash Sathe <yashsathe1357@gmail.com>
2026-08-09 11:33:57 +0530

    Completed Random Color generator

---
+++ unlimited-color/chaiaurcode.js
@@ -0,0 +1,25 @@
+const randomColor = function() {
+    const hex = "0123456789ABCDEF"
+    let color ="#"
+    for( let i =0 ; i <6; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+
+
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+
+let intevalId;
+
+start.addEventListener("click", () => {
+   const body = () =>{
+    document.querySelector("body").style.backgroundColor = randomColor()
+  }
+  intervalId = setInterval(body , 1000)
+});
+
+stop.addEventListener('click' , () =>{
+    clearInterval(intervalId);
+})
